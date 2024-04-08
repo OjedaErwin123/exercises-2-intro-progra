71acba7e145f70d2aa5a00c133bb9a234a9b872f
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-04-08 16:39:45 -0400

    119

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -279,7 +279,22 @@ void exercise_15(int decimal) {
 }
 
 void exercise_16(int divident, int divider) {
-  // TODO: YOUR CODE HERE 
+  // TODO: YOUR CODE HERE
+  int n,x,y,z;
+  x=divider;
+  y= divident;
+  z=0;
+   while(z<x){
+    z=z+y;
+    n++;
+      if(z>x){
+        z=z-y;
+        n--;
+        break;
+      }
+    }
+    x=x-z;
+    cout<<n<<" "<<x<<endl; 
 }
 
 void exercise_17(int n) {
