e2244ae359587050db62cce3fbcdd026372258bc
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-04-14 20:33:42 -0400

    192

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -104,22 +104,22 @@ void exercise_5(int n, int k) {
       calendar = " "x;
       break;
     case 2:
-      calendar = "   "x;
+      calendar = "  "x;
       break;
     case 3:
-      calendar = "       "x;
+      calendar = "    "x;
       break;
     case 4:
-      calendar = "         "x;
+      calendar = "       "x;
       break;
     case 5:
-      calendar = "           "x;
+      calendar = "         "x;
       break;
     case 6:
-      calendar = "             "x;
+      calendar = "           "x;
       break;
     case 7:
-      calendar = "               "x;
+      calendar = "             "x;
       break;
   }
   for(int i=1; i<=k; i++){
