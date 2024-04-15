0b7496ee849931edc1d035fd740f75aaae23d8e4
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-04-14 20:28:52 -0400

    190

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -101,25 +101,25 @@ void exercise_5(int n, int k) {
 
   switch(n){
     case 1:
-      calendar = " "x;
-      break;
-    case 2:
-      calendar = "  "x;
-      break;
-    case 3:
       calendar = "   "x;
       break;
+    case 2:
+      calendar = "   "x;
+      break;
+    case 3:
+      calendar = "       "x;
+      break;
     case 4:
-      calendar = "    "x;
+      calendar = "         "x;
       break;
     case 5:
-      calendar = "     "x;
+      calendar = "           "x;
       break;
     case 6:
-      calendar = "      "x;
+      calendar = "             "x;
       break;
     case 7:
-      calendar = "       "x;
+      calendar = "               "x;
       break;
   }
   for(int i=1; i<=k; i++){
